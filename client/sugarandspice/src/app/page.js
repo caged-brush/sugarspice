896056bc99d9ec211eed672f46135e85f25d6b00
Suleiman <suleimanj515@gmail.com>
2025-06-03 14:52:58 -0700

    hero section

--- client/sugarandspice/src/app/page.js
+++ client/sugarandspice/src/app/page.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "@/component/header";
+import Hero from "@/component/hero";
 
 const page = () => {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 };
---
+++ client/sugarandspice/src/component/hero.js
@@ -0,0 +1,50 @@
+"use client";
+
+import { useState } from "react";
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+
+const navigation = [
+  { name: "Product", href: "#" },
+  { name: "Features", href: "#" },
+  { name: "Marketplace", href: "#" },
+  { name: "Company", href: "#" },
+];
+
+export default function Hero() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <div className="grid grid-cols-2">
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+          <div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
+          <div className="text-center">
+            <h1 className="text-5xl font-semibold tracking-tight text-balance sm:text-7xl">
+              Data to enrich your online business
+            </h1>
+            <p className="mt-8 text-lg font-medium text-pretty  sm:text-xl/8">
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a
+                href="#"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Get started
+              </a>
+              <a href="#" className="text-sm/6 font-semibold text-gray-900">
+                Learn more <span aria-hidden="true">→</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center justify-center">
+        <img src="../assets/images/hero-image.jpg" className="rounded-4xl w-full h-[850px]"/>
+      </div>
+    </div>
+  );
+}

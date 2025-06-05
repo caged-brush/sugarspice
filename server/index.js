14f7f4bc4a926063c1e793726040498b38b26e37
Suleiman <suleimanj515@gmail.com>
2025-06-05 14:03:19 -0700

    bug fix

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 import express from "express";
-
+import axios from "axios";
+import pg from "pg";
+import bodyParser from "body-parser";
+import cors from "cors";
 const app = express();
-const PORT = 5000;
+const PORT = 3001;
+
+app.use(cors());
+
+app.get("/test", (req, res) => {
+  console.log("test");
+  res.send("Route is working");
+});
 
 app.listen(PORT, () => {
   console.log(`App now running on port ${PORT}`);

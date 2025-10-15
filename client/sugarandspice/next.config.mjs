d4540e923c461b98cd0803a3b0771321a9e4ae6a
Suleiman <suleimanj515@gmail.com>
2025-10-15 14:58:14 -0700

    UI improvemnet

--- client/sugarandspice/next.config.mjs
+++ client/sugarandspice/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  experimental: {
+    turbo: {
+      rules: {}, // use Turbopack
+    },
+  },
+};
 export default nextConfig;
